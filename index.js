760fdc50b154c860b7292c796c34e08a1aaf00a1
Dan Zemlyansky <danzemly000@gmail.com>
2024-12-05 15:07:18 +0200

    remove mobile

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const planeTrail = document.querySelector('.plane-trail');
-const planeImage = './Airplane-1.png';
-let plane = null;
-let lastX = 0, lastY = 0;
-let lastRotation = 0;
-let isTouchDevice = 'ontouchstart' in window || navigator.maxTouchPoints > 0; 
+const planeImage = './Airplane-1.png'; 
+let plane = null; 
+let lastX = 0, lastY = 0; 
+let lastRotation = 0; //store the last rotation value to smooth the transition
+
 document.body.style.cursor = `url(${planeImage}), auto`;
 
-function handleMovement(event) {
-  const { clientX: x, clientY: y } = event.touches ? event.touches[0] : event;
+document.addEventListener('mousemove', (event) => {
+  const { clientX: x, clientY: y } = event;
 
   if (!plane) {
     plane = document.createElement('img');
@@ -46,42 +46,7 @@ function handleMovement(event) {
 
   lastX = x;
   lastY = y;
-}
-
-document.addEventListener('mousemove', (event) => {
-  if (!isTouchDevice) {
-    handleMovement(event); 
-  }
 });
-
-document.addEventListener('touchmove', (event) => {
-  if (isTouchDevice) {
-    handleMovement(event); 
-  }
-}, { passive: true });
-
-function startDrag(event) {
-  event.preventDefault();
-
-  const { clientX: x, clientY: y } = event.touches ? event.touches[0] : event;
-  lastX = x;
-  lastY = y;
-
-  document.addEventListener('mousemove', handleMovement);
-  document.addEventListener('touchmove', handleMovement, { passive: true });
-}
-
-function stopDrag() {
-  document.removeEventListener('mousemove', handleMovement);
-  document.removeEventListener('touchmove', handleMovement);
-}
-
-document.addEventListener('mousedown', startDrag);
-document.addEventListener('touchstart', startDrag, { passive: true });
-
-document.addEventListener('mouseup', stopDrag);
-document.addEventListener('touchend', stopDrag, { passive: true });
-document.addEventListener('touchcancel', stopDrag, { passive: true });
 
 function createSmoke(x, y, deltaX, deltaY) {
   const smoke = document.createElement('div');
